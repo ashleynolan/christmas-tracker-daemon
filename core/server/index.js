df27b7124b7bb218342582d44751a0ab347733ac
Ashley Nolan <anolan@tmw.co.uk>
2014-11-27 10:06:38 +0000

    Adding a proper 404 page

--- core/server/index.js
+++ core/server/index.js
@@ -13,10 +13,17 @@ var server = {
 	init : function (app, config) {
 		app.set('showStackError', true);
 
+		app.set('views', 'core/client/views');
 		app.set('view engine', 'jade');
 		app.set("view options", { layout: false });
 
 		app.set('port', process.env.PORT || 3001);
+
+		// expose package.json to views
+		app.use(function (req, res, next) {
+			res.locals.pkg = pkg;
+			next();
+		});
 	}
 
 };
